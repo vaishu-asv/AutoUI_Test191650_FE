5dccaae3079eb3c319b304778fd829bc4c5aaf48
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-19 16:56:11 +0530

    Initial commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,28 @@
+import { AuthenticationGuard } from './guards/authentication.guard';
+import { QuickUiModule } from 'quick-ui';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedRoutingModule } from './shared-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+@NgModule({
+  providers: [
+    AuthenticationGuard
+  ],
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedRoutingModule
+  ,	QuickUiModule,
+],
+  exports: [
+    HeaderComponent
+  , FooterComponent],
+})
+export class SharedModule { }
